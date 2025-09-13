8a568bebc5731099dc8f99a7c6ea9b37205672f3
Lucan <chuganilucan@gmail.com>
2025-09-12 22:25:16 -0700

    Updated most Projects
    Added sections, titles, descriptions, thumbnails, and links to the majority of projects.

--- src/Components/Pages/Projects.jsx
+++ src/Components/Pages/Projects.jsx
@@ -6,6 +6,38 @@ function Projects() {
         <div className="content-wrapper" id="projects">
             <h3 id="projects-header">Projects</h3>
 
+            <h5>Competitive Robotics</h5>
+            <div className="project-wrapper">
+                <ProjectCard
+                    img_url='https://ik.imagekit.io/lucan/High%20Stakes.jpg' img_alt='Alt'
+                    title='Ringbot 2025'
+                    description='Ring stacking robot for VEX Push Back World Championship'
+                    link='https://youtu.be/YQq7Y45SYrE'
+                    tags={["VEX Robotics"]}
+                />
+                <ProjectCard
+                    img_url='https://ik.imagekit.io/lucan/Over%20Under.jpg' img_alt='Alt'
+                    title='Ballbot 2024'
+                    description='Triball shooting and grappling robot for VEX Over Under World Championship'
+                    tags={["VEX Robotics"]}
+                />
+                <ProjectCard
+                    img_url='https://ik.imagekit.io/lucan/Robot4.JPG' img_alt='Alt'
+                    title='Frisbot 2023'
+                    description='Disc launching robot with AI goal tracking for VEX Spin Up World Championship'
+                    link='https://youtu.be/Nb1d4bbiFl4'
+                    tags={["VEX Robotics"]}
+                />
+                <ProjectCard
+                    img_url='https://ik.imagekit.io/lucan/Pitching%20In.jpg' img_alt='Alt'
+                    title='Catabot 2022'
+                    description='Ball launching and climbing robot with for VEX IQ Pitching In World Championship'
+                    link='https://youtu.be/dgtBVSzvTzQ'
+                    tags={["VEX IQ Robotics"]}
+                />
+            </div>
+
+            <h5>Python Apps</h5>
             <div className="project-wrapper">
                 <ProjectCard
                     img_url='https://ik.imagekit.io/lucan/Password%20manager.png' img_alt='Alt'
@@ -36,12 +68,45 @@ function Projects() {
                     description='Graphical word guessing game with ulimited plays per day'
                     tags={["Python", "Tkinter"]}
                 />
+            </div>
+
+            <h5>Websites</h5>
+            <div className="project-wrapper">
                 <ProjectCard
-                    img_url='https://ik.imagekit.io/lucan/wordle.png'
+                    img_url='https://ik.imagekit.io/lucan/Space.jpg'
                     img_alt='Alt'
-                    title='Woodworking'
-                    description='Manufactured with various tools'
-                    tags={["Python", "Tkinter"]}
+                    title='Astronomy Picture of the Day'
+                    description="Website showcases daily space images and insights using NASA's API"
+                    link='https://apod-api.netlify.app'
+                    tags={["Javascript", "HTML", "CSS"]}
+                />
+                <ProjectCard
+                    img_url='https://ik.imagekit.io/lucan/Hands%20of%20Fate.png'
+                    img_alt='Alt'
+                    title='AI Blackjack Game'
+                    description="Gesture controlled website with image recognition model"
+                    link='https://hands-of-fate-blackjack.netlify.app'
+                    tags={["Google Teachable Machine", "Javascript"]}
+                />
+                <ProjectCard
+                    img_url='https://ik.imagekit.io/lucan/trivia-transformed.jpeg'
+                    img_alt='Alt'
+                    title='Trivia 101 Game'
+                    description="Online True or False game using Open Trivia Database API"
+                    link='https://trivia101.netlify.app'
+                    tags={["Javascript", "HTML", "CSS"]}
+                />
+            </div>
+
+            <h5>Craftsmanship</h5>
+            <div className="project-wrapper">
+                <ProjectCard
+                    img_url='https://ik.imagekit.io/lucan/3%20Woodwork%20Projects.png'
+                    img_alt='Alt'
+                    title='Woodwork Projects'
+                    description='Functional and decorative pieces made using different wood, tools, and techniques'
+                    link='https://photos.app.goo.gl/LuZRV7PdyhawKJDx9'
+                    tags={["Woodwork 10"]}
                 />
             </div>
         </div>
